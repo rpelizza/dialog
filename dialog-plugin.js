b51f2f245c45cdd2a43dc81bd2aed568c98586e3
Rafael Pedro Pelizza <rafael.pelizza@gmail.com>
2018-06-19 00:11:57 -0300

    verifica se o elemento tem id ou class ou ambos

--- dialog-plugin.js
+++ dialog-plugin.js
@@ -3,10 +3,13 @@
     if (!(/Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor))) {
         let dialogs = document.getElementsByTagName('dialog');
                 
-        for (let x = 0; x < dialogs.length; x++) {            
+        for (let x = 0; x < dialogs.length; x++) {
+            let elementClass = (dialogs[x].classList.length > 0) ? dialogs[x].className.split(' ') : null;
+            let elementID = (dialogs[x].id.length  > 0) ? dialogs[x].id : null;
+            let insideContent = dialogs[x].innerHTML;  
             let newElement = document.createElement('div');
-            let insideContent = dialogs[x].innerHTML;
-            newElement.setAttribute('id', dialogs[x].id);
+            if (elementClass) newElement.classList = elementClass.join(' ');
+            if (elementID) newElement.setAttribute('id', dialogs[x].id);
             newElement.innerHTML += insideContent;
             newElement.style.cssText = 'position:none;z-index:999;top:50%;left:50%;transform:translate(-50%,-50%)';
             document.body.appendChild(newElement);            
